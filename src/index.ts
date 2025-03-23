e2d7168b55886eda9d246867b8a59217c9d25716
Dimitris Stavroulakis <jimstav@gmail.com>
2025-03-23 12:58:14 +0200

    Linked List Implementation

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { CharactersCollection } from './CharactersCollection';
+import { LinkedList } from './LinkedList';
 import { NumbersCollection } from './NumbersCollection';
 import { Sorter } from './Sorter';
 
@@ -11,3 +12,13 @@ const charactersCollection = new CharactersCollection('Xaayb');
 const stringSorter = new Sorter(charactersCollection);
 stringSorter.sort();
 console.log(charactersCollection.data);
+
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(-3);
+linkedList.add(4);
+
+const linkedListSorter = new Sorter(linkedList);
+linkedListSorter.sort();
+linkedList.print();
