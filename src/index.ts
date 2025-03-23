b49162a73ba65289299439386458f94fbfbdada7
Dimitris Stavroulakis <jimstav@gmail.com>
2025-03-23 12:34:58 +0200

    Sorting Arbitrary Collections

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
+import { CharactersCollection } from './CharactersCollection';
 import { NumbersCollection } from './NumbersCollection';
 import { Sorter } from './Sorter';
 
 const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
-const sorter = new Sorter(numbersCollection);
-sorter.sort();
+const numbersSorter = new Sorter(numbersCollection);
+numbersSorter.sort();
 console.log(numbersCollection.data);
+
+const charactersCollection = new CharactersCollection('Xaayb');
+const stringSorter = new Sorter(charactersCollection);
+stringSorter.sort();
+console.log(charactersCollection.data);
